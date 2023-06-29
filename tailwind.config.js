56bb4006dc838d74d9374937aec523db6935c45e
gsngdavid <gsngdavid@gmail.com>
2023-06-29 10:54:31 +0200

    Workcation - tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+}
+
